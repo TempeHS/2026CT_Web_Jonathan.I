df453322658b0f0e398006593298222f3df29942
Jonathan I <jonathan.ip1@education.nsw.gov.au>
2025-03-11 00:56:10 +0000

    hi

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,54 @@
-player_name = prompt("enter your name");
-alert("i dont like " + player_name);
-alert("i dont like " + player_name);
-player_guess = prompt("Rock Paper Scissors");
-computer_guess = randonIntFromInterval(1, 3);
+gamerScore = "1";
+computerScore = 1;
 
-if (player_guess == computer_guess) {
-  document.getElementById("user_feedback").innerHTML = "<em>Correct</em>";
-} else {
-  document.getElementById("user_feedback").innerHTML =
-    "Incorrect\n" + "It was " + computer_guess;
-}
+sumScore = gamerScore + computerScore;
 
-function randonIntFromInterval(min, max) {
-  //min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
+alert(sumScore);
+
+function myGame() {
+  scissors = 1;
+  paper = 2;
+  rock = 3;
+
+  player_name = prompt("Enter your name:");
+  alert("Hello " + player_name);
+
+  player_guess = prompt("Choose scissors, paper, or rock:").toLowerCase();
+
+  computer_guess = randomIntFromInterval(1, 3);
+
+  let computer_choice = "";
+  if (computer_guess === scissors) computer_choice = "scissors";
+  if (computer_guess === paper) computer_choice = "paper";
+  if (computer_guess === rock) computer_choice = "rock";
+
+  if (
+    (player_guess === "scissors" && computer_guess === scissors) ||
+    (player_guess === "paper" && computer_guess === paper) ||
+    (player_guess === "rock" && computer_guess === rock)
+  ) {
+    document.getElementById("user_feedback").innerHTML =
+      "It's a tie! Both chose " + computer_choice;
+  } else if (
+    (player_guess === "scissors" && computer_guess === paper) ||
+    (player_guess === "paper" && computer_guess === rock) ||
+    (player_guess === "rock" && computer_guess === scissors)
+  ) {
+    document.getElementById("user_feedback").innerHTML =
+      "You win! Computer chose " + computer_choice;
+  } else if (
+    (player_guess === "scissors" && computer_guess === rock) ||
+    (player_guess === "paper" && computer_guess === scissors) ||
+    (player_guess === "rock" && computer_guess === paper)
+  ) {
+    document.getElementById("user_feedback").innerHTML =
+      "You lose! Computer chose " + computer_choice;
+  } else {
+    document.getElementById("user_feedback").innerHTML =
+      "Invalid input! Please enter scissors, paper, or rock.";
+  }
+
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
