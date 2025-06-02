5cd1f614d5487a21e200ead49ada2ce26d66a97e
Jonathan I <jonathan.ip1@education.nsw.gov.au>
2025-06-02 02:49:09 +0000

    Cool Stuff

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask (__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("KFC", "What is KFC?", "More", "static/images/card1.png"),
+        ("KFC", "What is KFC?", "More", "static/images/card1.png", "contact.html"),
         ("Homemade KFC", "How to make chicken", "", "static/images/card2.png"),
         ("KFC Resturaunts", "Description", "Button text", "static/images/card3.png"),
         ("Title", "Description", "Button text", "static/images/card4.png"),
@@ -16,6 +16,16 @@ def index():
     )
     return render_template("index.html", cards=card_data),200
 
- 
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html"), 200
+
+@app.route('/recipes.html')
+def recipes():
+    return render_template("recipes.html"), 200
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
